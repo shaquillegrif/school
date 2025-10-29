33b6a46aa327d2acd904103f07b00878805a24e6
sgriffith1995 <122536693+sgriffith1995@users.noreply.github.com>
2025-10-29 15:24:43 +0100

    Update G&S.php

--- G&S.php
+++ G&S.php
@@ -1,34 +1,44 @@
 <?php
-// define classes with methods
+// define class with methods
 class clsCalc
 {
-    public $valueX = 0;             // property valueX
-    public $valueY = 0;             // property valueY
+    public $valueX = 0;  // property valueX
+    public $valueY = 0;  // property valueY
 
-    public function setX($pX)           // setter of X
+    public function setX($pX)
     {
         $this->valueX = $pX;
     }
 
-    public function setY($pY)           // setter of Y
+    public function setY($pY)
     {
         $this->valueY = $pY;
     }
 
-    public function getSom()		    // getter Som
+    public function getSom()  // getter Som
     {
-        return (this->valueX + $this->valueY);
+        return $this->valueX + $this->valueY;
     }
 }
 
-$objectSom = new clsCalc();	    // make an object  
-$objectSom->setX(10);	        // set value of X (stored in property valueX)
-$objectSom->setY(2);	        // set value of Y (stored in property valueY)
-$objectSom->setY(3);	        // overrides previous value of Y (stored in property valueY)
-$renderSom = getSom();       // get calculated value of the som of X and Y 
+class clsVoetbalTeam
+{
+    private $clubnaam;
+
+    private $plaats;
+
+    private $teamcode;
+}
+// maak een object
+$objectSom = new clsCalc();
+$objectSom->setX(10);
+$objectSom->setY(3);  // waarde van Y overschrijft eerdere waarde
+
+// bereken de som
+$renderSom = $objectSom->getSom();
 ?>
 
-<!-- showing html and rendered variables -->
+<!-- HTML output -->
 <html>
 
 <body>
