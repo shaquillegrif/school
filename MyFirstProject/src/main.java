fddfc118bcd457a848ec5043156bdd7765f60ea9
sgriffith1995 <122536693+sgriffith1995@users.noreply.github.com>
2025-11-04 10:16:59 +0100

    update lesson 2

--- MyFirstProject/src/main.java
+++ MyFirstProject/src/main.java
@@ -19,9 +19,43 @@ public class main {
         int year = 2025;
         int quantity = 1;
 
+        double price = 19.99;
+        double gpa = 3.5;
+        double temperature = -12.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        String name ="Shaquille Griffith";
+
+
+
         System.out.println(age);
         System.out.println(year);
         System.out.println(quantity);
+
         System.out.println("the year is " + year);
+        System.out.println("$" + price);
+
+        System.out.println(grade);
+        System.out.println(symbol);
+        System.out.println(currency);
+
+        System.out.println(isStudent);
+        System.out.println(forSale);
+
+        if(isStudent == true){
+            System.out.println("You are a student");
+        }
+        else{
+            System.out.println("You are not a student");
+        }
+
+        System.out.println(name);
     }
 }
