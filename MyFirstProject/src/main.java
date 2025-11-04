02afffb97f74d64937600ad8dc1c7b0001dbc625
sgriffith1995 <122536693+sgriffith1995@users.noreply.github.com>
2025-11-04 09:55:19 +0100

    update lessons

--- MyFirstProject/src/main.java
+++ MyFirstProject/src/main.java
@@ -14,5 +14,14 @@ public class main {
         System.out.println("herru herru herru");
         System.out.println("ollah");
         System.out.println("I like trains!!!!!!");
+
+        int age = 30;
+        int year = 2025;
+        int quantity = 1;
+
+        System.out.println(age);
+        System.out.println(year);
+        System.out.println(quantity);
+        System.out.println("the year is " + year);
     }
 }
