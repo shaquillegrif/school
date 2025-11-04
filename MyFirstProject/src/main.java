2eb5a020a1acc79e3f9765f0d5a6d8288719a51b
sgriffith1995 <122536693+sgriffith1995@users.noreply.github.com>
2025-11-04 09:48:50 +0100

    first lesson done

---
+++ MyFirstProject/src/main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String [] args) {
+        //THIS IS MY FIRST JAVA PROGRAM
+        /*
+        this
+        is
+        a
+        multi-line
+        comment
+         */
+        System.out.println("I like pizza!");
+        System.out.println("It's really good!");
+        System.out.print("Buy me pizza!");
+        System.out.println("herru herru herru");
+        System.out.println("ollah");
+        System.out.println("I like trains!!!!!!");
+    }
+}
