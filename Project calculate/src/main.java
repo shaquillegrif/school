7983edc263afc5951181d24920c1b06a6a8e7d82
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2025-11-04 12:07:07 +0100

    area calculator

---
+++ Project calculate/src/main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter width: ");
+        width = scanner.nextDouble();
+        System.out.print("Enter height: ");
+        height = scanner.nextDouble();
+
+
+        area = width * height;
+        System.out.println("The area is: " + area);
+        scanner.close();
+    }
+}
