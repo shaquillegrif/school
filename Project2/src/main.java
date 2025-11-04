ff42a1938487069cc8a98b7738eb0b51b9b55cc7
sgriffith1995 <122536693+sgriffith1995@users.noreply.github.com>
2025-11-04 11:17:57 +0100

    Update main.java

--- Project2/src/main.java
+++ Project2/src/main.java
@@ -3,12 +3,30 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
 
-        Scanner scanner =  new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Please enter your name: ");
         String name = scanner.nextLine();
 
+        System.out.print("Please enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("What is your gpa: ");
+        double gpa = scanner.nextDouble();
+
+        System.out.print("Are you a student (true/false)?");
+        boolean isStudent = scanner.nextBoolean();
+
         System.out.println("Hello, " + name);
+        System.out.println("Your age is " + age + " years old.");
+        System.out.println("Your gpa is " + gpa);
+
+        if (isStudent) {
+            System.out.println("You are enrolled as a student.");
+        }
+        else{
+            System.out.println("You are not enrolled.");
+        }
         scanner.close();
     }
 }
