01656d5174f1b1f561c73bd70d17b4e01d182450
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2025-11-06 11:30:26 +0100

    update files

--- random numbers/src/main.java
+++ random numbers/src/main.java
@@ -6,15 +6,21 @@ public class main {
         Random random = new Random();
 
         int number1;
-        int number2;
-        int number3;
+        double number2;
+        boolean coinFlip;
 
         number1= random.nextInt(1, 7);
-        number2= random.nextInt(1, 7);
-        number3= random.nextInt(1, 7);
-        
+        number2= random.nextDouble(1, 7);
+        coinFlip = random.nextBoolean();
+
         System.out.println(number1);
         System.out.println(number2);
-        System.out.println(number3);
+
+        if(coinFlip){
+            System.out.println("Heads");
+        }
+        else{
+            System.out.println("Tails");
+        }
     }
 }
