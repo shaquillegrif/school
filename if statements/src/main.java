c264356765dcb2bf0bf9c6d47066994db2d6a4c3
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2025-11-06 10:45:49 +0100

    done

---
+++ if statements/src/main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        int age;
+        boolean isStudent;
+
+        System.out.print("Enter your name: ");
+        name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        age = scanner.nextInt();
+
+        System.out.print("Are you a student (true/false): ");
+        isStudent = scanner.nextBoolean();
+
+        //GROUP 1
+        if(name.isEmpty()){
+            System.out.print("You didn't enter a name! 🙁");
+        }
+        else{
+            System.out.println("Hello! "+name + "! 😁");
+        }
+
+        //GROUP 2
+
+        if (age >= 65) {
+            System.out.println("You are a senior");
+        }
+        else if  (age >= 18) {
+            System.out.println("You are an adult");
+        }
+        else if (age < 0) {
+            System.out.println("You haven't been born yet");
+
+        }
+        else if (age == 0) {
+            System.out.println("You are a baby");
+        }
+        else {
+            System.out.println("You are a child");
+        }
+
+        //GROUP 3
+
+        if (isStudent) {
+            System.out.println("You are a student");
+        }
+        else {
+            System.out.println("You are not a student");
+        }
+        scanner.close();
+    }
+}
