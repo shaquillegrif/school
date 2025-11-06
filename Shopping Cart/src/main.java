aad2d21f28deb5cd04726401ad6153cdbbc84109
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2025-11-06 10:05:49 +0100

    finished

---
+++ Shopping Cart/src/main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
+
+        System.out.print("What item would you like to buy?: ");
+        item = scanner.nextLine();
+
+        System.out.print("What is the price for each?: ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many would you like?: ");
+        quantity = scanner.nextInt();
+
+        total = price * quantity;
+
+        System.out.println( "You have bought " + quantity + " "+ item + "'s");
+        System.out.println("the "+ item + "'s " + "will cost " + currency + total);
+
+        scanner.close();
+    }
+
+}
