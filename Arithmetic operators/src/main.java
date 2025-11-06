e7881f743b95f0c49e0ab381848bfff774c88044
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2025-11-06 09:52:33 +0100

    done

---
+++ Arithmetic operators/src/main.java
@@ -0,0 +1,30 @@
+public class main {
+    public static void main(String[] args) {
+        // order of operations [P-E-M-D-A-S]
+
+
+        int x = 10;
+        int y = 3;
+        int z;
+
+        double result = 2 + 4 * (7 - 5) / 2.0;
+
+        //z = x + y;
+        //z = x - y;
+        //z = x * y;
+        //z = x / y;
+        //z = x % y;
+
+        //x =  x + y;
+        //x += y;
+        //x -= y;
+        //x*= y;
+
+        //x++;
+        //x--;
+
+        //System.out.println(z);
+        //System.out.println(x);
+        System.out.println(result);
+    }
+}
