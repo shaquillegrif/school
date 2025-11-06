6d09599cc0a267212ad07001118344e5328a2638
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2025-11-06 12:42:29 +0100

    update

--- Math Class/src/Main.java
+++ Math Class/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        double radius;
+        double circumference;
+        double area;
+        double volume;
+
+        System.out.print("Enter the radius: ");
+        radius = scanner.nextDouble();
+
+        circumference = 2 * Math.PI * radius;
+        area = Math.PI * Math.pow(radius, 2);
+        volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+
+        System.out.printf("The circumference is: %.1f cm%n", circumference);
+        System.out.printf("The area is: %.1f cm²%n", area);
+        System.out.printf("The volume is: %.1f cm³%n", volume);
+
+        scanner.close();
     }
 }
