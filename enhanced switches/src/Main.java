66aa3af24b07c14c9cadf3494fbbc83144baee5f
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2025-11-18 14:27:27 +0100

    update program

--- enhanced switches/src/Main.java
+++ enhanced switches/src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
+    public static String capitalizeAndFormat(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+
+        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+    }
+
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter day of week: ");
+        String userInput = scanner.nextLine();
+
+        String day = capitalizeAndFormat(userInput);
+
+
+        switch (day) {
+            case "Monday" -> System.out.println("It is a weekday");
+            case "Tuesday" -> System.out.println("It is a weekday");
+            case "Wednesday" -> System.out.println("It is a weekday");
+            case "Thursday" -> System.out.println("It is a weekday");
+            case "Friday" -> System.out.println("It is a weekday");
+            case "Saturday" -> System.out.println("It is the weekend");
+            case "Sunday" -> System.out.println("It is the weekend");
+            default -> System.out.println(day + " is not a day");
+        }
+        scanner.close();
     }
 }
