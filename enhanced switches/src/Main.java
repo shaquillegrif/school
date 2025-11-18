280b753d071e3188853ea92547fcde778f17a62a
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2025-11-18 14:30:22 +0100

    cleaning up

--- enhanced switches/src/Main.java
+++ enhanced switches/src/Main.java
@@ -20,13 +20,8 @@ public class Main {
 
 
         switch (day) {
-            case "Monday" -> System.out.println("It is a weekday");
-            case "Tuesday" -> System.out.println("It is a weekday");
-            case "Wednesday" -> System.out.println("It is a weekday");
-            case "Thursday" -> System.out.println("It is a weekday");
-            case "Friday" -> System.out.println("It is a weekday");
-            case "Saturday" -> System.out.println("It is the weekend");
-            case "Sunday" -> System.out.println("It is the weekend");
+            case "Monday","Tuesday","Wednesday","Thursday","Friday" -> System.out.println("It is a weekday");
+            case "Saturday", "Sunday" -> System.out.println("It is the weekend");
             default -> System.out.println(day + " is not a day");
         }
         scanner.close();
