a993a4465aee42b3deb33baeaa652dd41fd9948a
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2025-11-26 11:14:24 +0100

    update

--- While loop/src/Main.java
+++ While loop/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = "";
+        String response = "";
+
+        while (name.isEmpty()){
+            System.out.print("Please enter your name: ");
+            name = scanner.nextLine();
+        }
+
+        System.out.println("Hello, " + name + "!");
+
+        while (!response.equals("Q") ){
+            System.out.println("you are playing a game");
+            System.out.println("Press Q to quit");
+            response = scanner.nextLine().toUpperCase();
+        }
+        System.out.println("you have quit the game");
+
+        scanner.close();
     }
 }
