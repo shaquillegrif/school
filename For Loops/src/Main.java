20ff9c65aff35ba15104be3407571e09d48a8341
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2025-11-27 13:41:31 +0100

    update to for loops

--- For Loops/src/Main.java
+++ For Loops/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        for (int i = 1; i <= 10;  i++) {
+            System.out.println(i);
+        }
+
+        for (int i = 10; i > 0;  i--) {
+            System.out.println(i);
+        }
     }
 }
