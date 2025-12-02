65df6a4f849e80df5f1f09a6d4331bc8ce931795
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2025-12-02 14:22:32 +0100

    first set-up

--- nested loops/src/Main.java
+++ nested loops/src/Main.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int rows;
+        int columns;
+        char symbol;
 
-        for (int i = 1; i <= 9; i++) {
-            System.out.println(i);
+        System.out.print("Enter the number of rows: ");
+        rows = scanner.nextInt();
+
+        System.out.print("Enter the number of columns: ");
+        columns = scanner.nextInt();
+
+        System.out.print("Enter the symbol: ");
+        symbol = scanner.next().charAt(0);
+
+        for (int i = 0; i <= columns; i++) {
+            System.out.print(symbol);
         }
+
+        scanner.close();
     }
 }
