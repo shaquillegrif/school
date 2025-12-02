90f6bcf16e8d9c3dbd7a0528ee89e75d3de5967c
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2025-12-02 13:59:23 +0100

    update to code

--- For Loops/src/Main.java
+++ For Loops/src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
 
         for (int i = 1; i <= 10;  i++) {
             System.out.println(i);
@@ -8,5 +12,26 @@ public class Main {
         for (int i = 10; i > 0;  i--) {
             System.out.println(i);
         }
+
+        for (int i = 10; i > 0;  i--) {
+            System.out.println("pizza");
+        }
+
+        System.out.print("Enter how many times you wanna loop: ");
+        int max = scanner.nextInt();
+
+        for (int i = 1; i <= max; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("How many seconds to countdown from: ");
+        int start = scanner.nextInt();
+
+        for (int i = start; i > 0; i--) {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+        System.out.println("Happy new year");
+        scanner.close();
     }
 }
