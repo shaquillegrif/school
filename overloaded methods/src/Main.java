52f571ea1f1d1db7660b505715c8426304a55469
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2025-12-17 12:02:20 +0100

    update

--- overloaded methods/src/Main.java
+++ overloaded methods/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        System.out.println(add (1, 2));
+        System.out.println(add(1, 2, 3));
+
+    }
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+    static double add(double a, double b, double c) {
+        return a + b + c;
     }
 }
