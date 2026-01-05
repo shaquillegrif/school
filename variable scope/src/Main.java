f71f1cb94ce2256a52daf6495d3a108d729fa4b7
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-01-05 11:12:11 +0100

    create project variable scope

---
+++ variable scope/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int x =1;
+
+        System.out.println(x);
+        doSomething();
+    }
+    static void doSomething(){
+        int x =2;
+
+        System.out.println(x);
+    }
+    static int x = 3; 
+}
