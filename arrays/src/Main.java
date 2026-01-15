776ebb0bdede75d9c1a92824980616799198213b
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-01-15 14:08:06 +0100

    update and finish

--- arrays/src/Main.java
+++ arrays/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        String[] fruits = {"apple", "orange", "banana", "coconut"};
+        //fruits[0] = "pineapple";
+        int numOfFruits = fruits.length;
+
+        System.out.println(fruits[0]);
+        System.out.println(numOfFruits);
+
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println(fruits[i]);
+        }
+        Arrays.sort(fruits);
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+        Arrays.fill(fruits, "pinapple");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+
     }
 }
