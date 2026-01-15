3c7c6d67ab89eb3f1ce7d5da48813c9e39e9888f
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-01-15 14:52:39 +0100

    update and finish

--- user input arrays/src/Main.java
+++ user input arrays/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String [] foods;
+        int size;
+
+        System.out.print("what number of food would you like: ");
+        size = scanner.nextInt();
+        scanner.nextLine();
         
+        foods = new String[size];
+
+        for (int i = 0; i < foods.length; i++){
+            System.out.print("Enter a food: ");
+            foods[i] = scanner.nextLine();
+        }
+
+        for(String food : foods){
+            System.out.println(food);
+        }
+
+
+        scanner.close();
     }
 }
