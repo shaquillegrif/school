8ed42dd1ef99601ed2fd48851d118a6462f6f56e
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-01-20 12:14:46 +0100

    finish project

--- 2d array/src/Main.java
+++ 2d array/src/Main.java
@@ -1,25 +1,37 @@
 public class Main {
     public static void main(String[] args) {
 
+
+        char[][] telephone = {{'1', '2', '3'},
+                              {'4', '5', '6'},
+                              {'7', '8', '9'},
+                              {'*', '0', '#'}};
+
+        for(char[] row: telephone){
+            for(char number: row){
+                System.out.print(number+ " ");
+            }
+            System.out.println();
+
+        }
+    }
+}
+
 //        String[] fruits = {"apple", "Banana", "Orange"};
 //        String[] vegetables = {"potato", "onion", "carrot"};
 //        String[] meats = {"chicken", "pork", "beef", "fish"};
 
 //        String[][] groceries = {fruits, vegetables, meats};
-        String[][] groceries = {{"apple", "Banana", "Orange"},
-                                {"potato", "onion", "carrot"},
-                                {"chicken", "pork", "beef", "fish"}};
 
-        groceries[0][0] = "pineapple";
-        groceries[1][0] = "celery";
-        groceries[1][2] = "cheese";
-        groceries[2][1] = "eggs";
+//String[][] groceries = {{"apple", "Banana", "Orange"},
+//        {"potato", "onion", "carrot"},
+//        {"chicken", "pork", "beef", "fish"}};
 
-        for (String[] foods: groceries){
-            for (String food: foods){
-                System.out.print(food + " ");
-            }
-            System.out.println();
-        }
-    }
-}
+//groceries[0][0] = "pineapple";
+//groceries[1][0] = "celery";
+//groceries[1][2] = "cheese";
+//groceries[2][1] = "eggs";
+//
+//        for (String[] foods: groceries){
+//        for (String food: foods){
+//        System.out.print(food + " ");
