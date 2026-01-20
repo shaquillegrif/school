97a2f70b5fd833e3e46345def40f105077ecb149
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-01-20 10:41:36 +0100

    update main.java

--- search an array/src/Main.java
+++ search an array/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        int[] numbers = {1, 9, 2, 8, 3, 5, 4};
+        int target = 4;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if  (target == numbers[i]) {
+                System.out.println("Element found at index " + i);
+            }
+        }
     }
 }
