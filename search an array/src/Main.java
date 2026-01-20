3f264018863e1d6a08e1a125efb58ae26c610599
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-01-20 11:19:19 +0100

    put in search function and finish

--- search an array/src/Main.java
+++ search an array/src/Main.java
@@ -1,13 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        int[] numbers = {1, 9, 2, 8, 3, 5, 4};
-        int target = 4;
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < numbers.length; i++) {
-            if  (target == numbers[i]) {
+        int[] numbers = {1, 9, 2, 8, 3, 5, 4};
+        String[] fruits = {"apple", "orange", "banana"};
+        boolean isFound = false;
+        String target;
+
+        System.out.print("Enter a fruit to search for: ");
+        target = scanner.nextLine();
+//        int target = 1;
+
+
+        for (int i = 0; i < fruits.length; i++) {
+            if (fruits[i].equals(target)) {
                 System.out.println("Element found at index " + i);
+                isFound = true;
+                break;
             }
         }
+
+
+        if (!isFound) {
+            System.out.println("Element not found");
+        }
+//        if (!isFound) {
+//            System.out.println("Element not found");
+//        }
+//        for (int i = 0; i < fruits.length; i++) {
+//            if  (target == numbers[i]) {
+//                System.out.println("Element found at index " + i);
+//                isFound = true;
+//                break;
+//            }
+//        }
+        scanner.close();
     }
 }
