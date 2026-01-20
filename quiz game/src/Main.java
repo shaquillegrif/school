15a5fd581cb4a3390f6cff070b12bc9393548326
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-01-20 13:55:21 +0100

    finish the game

--- quiz game/src/Main.java
+++ quiz game/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
                               "Who is know as the father of computers?",
                               "What was the first programming language?"};
 
-        String[][] option = {{"1. storing files", "2. encrypting data", "3. directing data", "4. managing passwords"},
+        String[][] options = {{"1. storing files", "2. encrypting data", "3. directing data", "4. managing passwords"},
                              {"1. CPU", "2. hard drive", "3. RAM", "4. GPU"},
                              {"1. 2000", "2. 2004", "3. 2006", "4. 2008"},
                              {"1. Steve Jobs", "2. Bill Gates", "3. Alan Turning", "4. Charles Babbage"},
@@ -24,6 +24,28 @@ public class Main {
         System.out.println("Welcome to the java quiz game!");
         System.out.println("******************************");
 
+        for(int i = 0; i < questions.length; i++) {
+            System.out.println(questions[i]);
+
+            for (String option : options[i]){
+                System.out.println(option);
+            }
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+
+            if(guess == answers[i]){
+                System.out.println("*******");
+                System.out.println("correct");
+                System.out.println("*******");
+                score++;
+            }
+            else {
+                System.out.println("*********");
+                System.out.println("incorrect");
+                System.out.println("*********");
+            }
+        }
+        System.out.println("Your final score is: " + score + " out of " + questions.length + " questions");
         scanner.close();
     }
 }
