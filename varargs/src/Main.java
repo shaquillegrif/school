3dff72efbbb1de505bb1435581d5394d93ae1793
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-01-20 11:33:48 +0100

    try out

--- varargs/src/Main.java
+++ varargs/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        System.out.println(add(1, 2, 3, 4));
+    }
+    static int add(int... numbers) {
+
+        int sum = 0;
+
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
     }
 }
