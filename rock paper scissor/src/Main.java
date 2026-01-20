10475241e39a8c4dbf3e7a0ec67afe5678d78405
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-01-20 14:05:38 +0100

    first setup

--- rock paper scissor/src/Main.java
+++ rock paper scissor/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] choices = {"rock", "paper", "scissor"};
+        String playersChoice;
+        String computerChoice;
+        String playAgain = "yes";
+
+        System.out.println("Enter your move (rock, paper, scissor): ");
+        playersChoice = scanner.nextLine(). toLowerCase();
         
+
+
+
+
+        scanner.close();
     }
 }
