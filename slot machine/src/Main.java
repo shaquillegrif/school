46e48c74efc6e6c92e3c5e9c52a1da40caed74a1
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-01-21 13:48:01 +0100

    first testing

--- slot machine/src/Main.java
+++ slot machine/src/Main.java
@@ -1,5 +1,58 @@
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int balance = 100;
+        int bet;
+        int payout;
+        String[] row;
+
+        System.out.println("************************");
+        System.out.println("  welcome to java slots ");
+        System.out.println("Symbols: 🍒 🍉 🍋 🔔 ⭐");
+        System.out.println("************************");
+
+        while (balance>0){
+            System.out.println("Current balance is : $ "+balance);
+            System.out.print("Place your bet amount: ");
+            bet = scanner.nextInt();
+            if(bet > balance){
+                System.out.println("INSUFFICIENT FUNDS");
+                continue;
+            }
+            else if(bet <= 0){
+                System.out.println("Bet must be greater than 0");
+            }
+            else {
+                balance -= bet;
+            }
+            System.out.println("Spinning");
+            row = spinRow();
+            printRow(row);
+            getPayout(row, bet);
+        }
+
+        scanner.close();
+    }
+    static String[] spinRow(){
+        String[] symbols = {"🍒", "🍉", "🍋", "🔔", "⭐"};
+        String[] row = new String[3];
+        Random random = new Random();
+
+        for(int i=0; i< 3;i++){
+            row[i] = symbols[random.nextInt(symbols.length)];
+        }
+
+        return row;
+    }
+    static void printRow(String[] row){
+        System.out.println(" " + String.join(" | ",  row));
+    }
+    static void getPayout(String[] row, int bet){
+
+       
     }
 }
