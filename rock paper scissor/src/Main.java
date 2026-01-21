517a0a94e0b0b105e8029d3132fb1d07e611e6cd
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-01-21 12:23:15 +0100

    finish project

--- rock paper scissor/src/Main.java
+++ rock paper scissor/src/Main.java
@@ -11,13 +11,36 @@ public class Main {
         String computerChoice;
         String playAgain = "yes";
 
-        System.out.println("Enter your move (rock, paper, scissor): ");
-        playersChoice = scanner.nextLine(). toLowerCase();
-        
+        do {
+            System.out.print("Enter your move (rock, paper, scissor): ");
+            playersChoice = scanner.nextLine(). toLowerCase();
 
+            while (!playersChoice.equals("rock") &&
+                    !playersChoice.equals("paper")&&
+                    !playersChoice.equals("scissor")) {
+                System.out.print("Invalid choice. Try again: ");
+                playersChoice = scanner.nextLine();
+            }
 
+            computerChoice = choices[random.nextInt(choices.length)];
+            System.out.println("Computer choice: " + computerChoice);
+            if(playersChoice.equals(computerChoice)) {
+                System.out.println("it's a tie");
+            }
+            else if((playersChoice.equals("rock") && computerChoice.equals("scissor"))||
+                    (playersChoice.equals("paper") && computerChoice.equals("rock"))||
+                    (playersChoice.equals("scissor") && computerChoice.equals("paper"))){
+                System.out.println("you win");
+            }
+            else{
+                System.out.println("you lose");
+            }
+            System.out.print("play again? (yes/no)");
+            playAgain = scanner.nextLine().toLowerCase();
 
+        }while (playAgain.equals("yes"));
 
+        System.out.println("thanx for playing");
         scanner.close();
     }
 }
