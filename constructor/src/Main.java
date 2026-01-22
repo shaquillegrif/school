3e5f2d4e6fc9207e451055f37c3b39a975c486db
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-01-22 12:23:18 +0100

    update

--- constructor/src/Main.java
+++ constructor/src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        Student student1 = new Student("spongebob", 30, 3.2);
+        Student student2 = new Student("patrick", 34, 1.5);
+        Student student3 = new Student("sandy", 27, 4.0);
+
+        System.out.println(student1.name);
+        System.out.println(student1.age);
+        System.out.println(student1.gpa);
+        System.out.println(student1.isEnrolled);
+        student1.study();
+
+
+        System.out.println(student2.name);
+        System.out.println(student2.age);
+        System.out.println(student2.gpa);
+        System.out.println(student2.isEnrolled);
+        student2.study();
+
+        System.out.println(student3.name);
+        System.out.println(student3.age);
+        System.out.println(student3.gpa);
+        System.out.println(student3.isEnrolled);
+        student3.study();
     }
 }
