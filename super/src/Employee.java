9ee1a7b35446868c248c52951afe26e9047a5331
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-01-28 11:09:46 +0100

    create employee.java

---
+++ super/src/Employee.java
@@ -0,0 +1,9 @@
+public class Employee extends Person {
+
+    int salary;
+
+    Employee(String first, String last, int salary) {
+        super(first, last);
+        this.salary = salary;
+    }
+}
