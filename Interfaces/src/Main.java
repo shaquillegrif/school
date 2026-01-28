387cea5c3ef6ea1ba4e8c8e64ded07893bb3958a
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-01-28 14:22:13 +0100

    update

--- Interfaces/src/Main.java
+++ Interfaces/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        Rabbit rabbit = new Rabbit();
+        Hawk hawk = new Hawk();
+
+
+        rabbit.flee();
+        hawk.hunt();
     }
 }
--- Interfaces/src/Rabbit.java
+++ Interfaces/src/Rabbit.java
@@ -1,2 +1,7 @@
-public class Rabbit {
+public class Rabbit implements Prey {
+
+    @Override
+    public void flee(){
+        System.out.println("the rabbit is running away");
+    }
 }
