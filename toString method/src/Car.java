53e5420344345eb1396b48451adff2d4883accdd
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-01-28 11:45:56 +0100

    create project and classes

---
+++ toString method/src/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+
+    String make;
+    String model;
+    int year;
+    String color;
+
+    Car(String make, String model, int year, String color) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+}
---
+++ toString method/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car("ford", "mustang", 2025, "red");
+
+        System.out.println(car);
+
+    }
+}
