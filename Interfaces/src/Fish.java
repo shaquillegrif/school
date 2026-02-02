851489168db9041ee1f874329545c926bc966ed8
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-02 10:47:03 +0100

    update Fish.java

--- Interfaces/src/Fish.java
+++ Interfaces/src/Fish.java
@@ -1,2 +1,12 @@
-public class Fish {
+public class Fish implements Prey, Predator{
+
+    @Override
+    public void flee() {
+        System.out.println("Fish fleeing");
+    }
+
+    @Override
+    public void hunt(){
+        System.out.println("Fish hunting");
+    }
 }
