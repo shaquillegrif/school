f07b12eb2ecd3d9264871b9d23795e5df979f3ba
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-02 14:25:39 +0100

    update main.java

--- Composition/src/Main.java
+++ Composition/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        Car car = new Car("mustang", 1969, "V8");
+
+        System.out.println(car.model);
+        System.out.println(car.year);
+        System.out.println(car.engine.type);
     }
 }
