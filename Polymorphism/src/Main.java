4ea8e0a9fa1a8d9f9fce5d4052c9ffb93f97d51b
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-02 11:05:14 +0100

    update Main.java

--- Polymorphism/src/Main.java
+++ Polymorphism/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        Car car = new Car();
+        Bike bike = new Bike();
+        Boat boat = new Boat();
+
+        Vehicle[]vehicles = {car,bike,boat};
+
+        for (Vehicle vehicle : vehicles) {
+            vehicle.go(); 
+        }
     }
 }
