6ccd22eb2081134c2fa8f55056db4684f8950085
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-02 13:20:38 +0100

    update

---
+++ Aggregation/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Book book1 = new Book("The fellowship of the ring" , 423);
+        Book book2 = new Book("The two towers" , 352);
+        Book book3 = new Book("The return of the king" , 416);
+
+        Book[] books = {book1, book2, book3};
+
+        for (Book book : books) {
+            System.out.println(book.displayInfo());
+        }
+    }
+}
