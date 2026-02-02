d507822cd1cf64d577b5b3ffbf5cfd5bcf8b515d
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-02 12:37:57 +0100

    create project and first functions

---
+++ Getters-Setters/src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+
+    String model;
+    String color;
+    int price;
+
+    Car(String model, String color, int price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+}
--- Getters-Setters/src/Main.java
+++ Getters-Setters/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        Car car = new Car("charger", "black", 1900000);
+        System.out.println(car.color + " " + car.model + " " + car.price);
     }
 }
