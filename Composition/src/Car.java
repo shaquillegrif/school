5b75c63886f9e63b416061ee39bf6d68a5b23d80
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-02 14:23:44 +0100

    update files car engine and main

---
+++ Composition/src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+
+    String model;
+    int year;
+    Engine engine;
+
+    Car(String model, int year, String engineType) {
+        this.model = model;
+        this.year = year;
+        this.engine = new Engine(engineType);
+    }
+}
---
+++ Composition/src/Engine.java
@@ -0,0 +1,8 @@
+public class Engine {
+
+    String type;
+
+    Engine(String type) {
+        this.type = type;
+    }
+}
