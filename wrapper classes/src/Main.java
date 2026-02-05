0b8580fb69f057fc9dda468776b1c782c740d994
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-05 12:58:23 +0100

    update main.java

--- wrapper classes/src/Main.java
+++ wrapper classes/src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+
+        // Autoboxing
+        Integer a = 123;
+        Double b = 3.14;
+        Character c = '$';
+        Boolean d = true;
+
+
+        // Unboxing
+
+        int x = a;
+        double y = b;
+        char w = c;
+        boolean z = d;
+
+        String a1 = Integer.toString(123);
+        String a2 = Double.toString(3.14);
+        String a3 = Character.toString('$');
+        String a4 = Boolean.toString(true);
+
     }
 }
