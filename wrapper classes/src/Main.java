cd1a70e2d64906ea8b69f0aed736595bd87ff9a7
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-10 11:13:34 +0100

    update wrapper.main and create new project

--- wrapper classes/src/Main.java
+++ wrapper classes/src/Main.java
@@ -2,28 +2,38 @@ public class Main {
     public static void main(String[] args) {
 
 
-        // Autoboxing
-        Integer a = 123;
-        Double b = 3.14;
-        Character c = '$';
-        Boolean d = true;
+
+        int a = Integer.parseInt("123");
+        double b = Double.parseDouble("3.14");
+        char c = "pizza".charAt(0);
+        boolean d = Boolean.parseBoolean("true");
 
 
-        // Unboxing
-
-        int x = a;
-        double y = b;
-        char w = c;
-        boolean z = d;
-
-        String a1 = Integer.toString(123);
-        String a2 = Double.toString(3.14);
-        String a3 = Character.toString('@');
-        String a4 = Boolean.toString(false);
+//
+//        // Autoboxing
+//        Integer a = 123;
+//        Double b = 3.14;
+//        Character c = '$';
+//        Boolean d = true;
 
 
-        String xn = a1 + a2 + a3 + a4;
-        System.out.println(xn);
+//
+//
+//        // Unboxing
+//
+//        int x = a;
+//        double y = b;
+//        char w = c;
+//        boolean z = d;
+//
+//        String a1 = Integer.toString(123);
+//        String a2 = Double.toString(3.14);
+//        String a3 = Character.toString('@');
+//        String a4 = Boolean.toString(false);
+//
+//
+//        String xn = a1 + a2 + a3 + a4;
+//        System.out.println(xn);
 
     }
 }
