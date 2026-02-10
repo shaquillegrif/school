a12a2cc973c184af4433894f794d6d48f123d43a
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-10 11:44:44 +0100

    finish main.java

--- exception handeling/src/Main.java
+++ exception handeling/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+
+
+
+        try( Scanner scanner = new Scanner(System.in)){
+            System.out.print("Enter a number: ");
+            int num = scanner.nextInt();
+            System.out.println("Entered number is: " + num);
+        }
+        catch(InputMismatchException e){
+            System.out.println("Invalid input");
+        }
+
+        catch(ArithmeticException e){
+            System.out.println("you can't divide by zero");
+        }
+        catch(Exception e){
+            // safety net
+            System.out.println("error something went wrong");
+        }
+        finally{
+            System.out.println("this always executes");
+        }
     }
 }
