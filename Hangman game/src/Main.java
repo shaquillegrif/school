92686abe0a75008022b45575fb140982bd877603
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-11 12:15:56 +0100

    added random words function

--- Hangman game/src/Main.java
+++ Hangman game/src/Main.java
@@ -1,10 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        String word = "pizza";
+        String filepath = "words.txt";
+        ArrayList<String> words = new ArrayList<>();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filepath))){
+            String line;
+            while((line = reader.readLine()) != null){
+                words.add(line.trim());
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
+        catch (IOException e){
+            System.out.println("something went wrong");
+        }
+
+        Random random = new Random();
+
+        String word = words.get(random.nextInt(words.size()));
+        
+
         Scanner scanner = new Scanner(System.in);
         ArrayList<Character> wordstate = new ArrayList<>();
         int wrongGuesses = 0;
