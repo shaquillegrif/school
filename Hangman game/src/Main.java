83b51a36816c2e69f1e6cd1e3abbb153516d6589
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-11 11:01:12 +0100

    test art for wrong guesses

--- Hangman game/src/Main.java
+++ Hangman game/src/Main.java
@@ -1,5 +1,64 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        String word = "pizza";
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Character> wordstate = new ArrayList<>();
+        int wrongGuesses = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            wordstate.add('-');
+        }
+        System.out.println("##################");
+        System.out.println("welcome to Hangman");
+        System.out.println("##################");
+
+        System.out.println(getHangmanArt(0));
+
+        scanner.close();
+    }
+    static String getHangmanArt(int wrongGuesses){
+
+        return switch (wrongGuesses){
+            case 0 -> """
+                      
+                      
+                      
+                      """;
+            case 1 -> """
+                       o
+                      
+                      
+                      """;
+            case 2 -> """
+                       o
+                       |
+                      
+                      """;
+            case 3 -> """
+                       o
+                      /|
+                      
+                      """;
+            case 4 -> """
+                       o
+                      /|\\
+                      
+                      """;
+            case 5 -> """
+                       o
+                      /|\\
+                      /
+                      """;
+            case 6 -> """
+                       o
+                      /|\\
+                      / \\
+                      """;
+            default -> "";
+        };
     }
 }
