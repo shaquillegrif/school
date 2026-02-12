5baf4dfd09de6bb9a88abbd28c6140752f7d9b90
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-12 11:43:59 +0100

    update main.java

--- Date and time/src/Main.java
+++ Date and time/src/Main.java
@@ -1,14 +1,34 @@
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
 
         LocalDate date = LocalDate.now();
         LocalTime time = LocalTime.now();
+        LocalDateTime dateTime = LocalDateTime.now();
+        Instant instant = Instant.now();
+        LocalDate date2 = LocalDate.of(2024, 12, 25);
+        LocalDateTime dateTime1 = LocalDateTime.of(2023, 12, 25, 12, 12);
+        LocalDateTime dateTime2 = LocalDateTime.of(2024, 12, 25, 12, 12);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        String newDateTime = dateTime.format(formatter);
 
         System.out.println(date);
         System.out.println(time);
+        System.out.println(dateTime);
+        System.out.println(instant);
+        System.out.println(newDateTime);
+        System.out.println(date2);
+        System.out.println(dateTime2);
 
+        if(dateTime1.isBefore(dateTime2)) {
+            System.out.println(dateTime1 + " is before " + dateTime2);
+        }
     }
 }
