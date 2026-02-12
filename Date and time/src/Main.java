eb7625ed1af5416d6927196a24595aff3710f2a4
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-12 11:00:35 +0100

    update program

---
+++ Date and time/src/Main.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+
+        System.out.println(date);
+        System.out.println(time);
+
+    }
+}
--- Hangman game/src/Main.java
+++ Hangman game/src/Main.java
@@ -28,7 +28,7 @@ public class Main {
         Random random = new Random();
 
         String word = words.get(random.nextInt(words.size()));
-        
+
 
         Scanner scanner = new Scanner(System.in);
         ArrayList<Character> wordstate = new ArrayList<>();
