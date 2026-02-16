a7acec5d982bdd42c0abb3b5a8fb6d27c3e4be64
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-16 11:28:19 +0100

    finish project

--- TimerTask/src/Main.java
+++ TimerTask/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Timer timer = new Timer();
+        TimerTask Task = new TimerTask() {
+
+            int count = 3;
+
+            @Override
+            public void run() {
+                System.out.println("Hello World");
+                count--;
+                if (count <= 0) {
+                    System.out.println("Task complete");
+                    timer.cancel();
+                }
+            }
+        };
+
+        timer.schedule(Task, 3000, 1000);
     }
 }
