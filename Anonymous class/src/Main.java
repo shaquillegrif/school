d7cc72bd257ab75827d47603c0ac7006d60d57be
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-16 11:16:58 +0100

    finish

--- Anonymous class/src/Main.java
+++ Anonymous class/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog(){
+            @Override
+             void speak() {
+                System.out.println("Scooby Doo says Ruh Roh");
+            }
+        };
+
+        dog1.speak();
+        dog2.speak();
     }
 }
