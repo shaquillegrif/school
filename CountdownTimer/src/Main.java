9ff98550b5614f4c496d5e804fb8f0418b356080
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-02-16 11:45:38 +0100

    finish project

--- CountdownTimer/src/Main.java
+++ CountdownTimer/src/Main.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the number of seconds you want to countdown from: ");
+        int response = scanner.nextInt();
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            int count = response;
+            @Override
+            public void run() {
+                System.out.println(count);
+                count--;
+               if (count < 0) {
+                   System.out.println("happy new year");
+                   timer.cancel();
+               }
+            }
+        };
+        timer.scheduleAtFixedRate(task, 0, 1000);
+        scanner.close();
+    }
 }
