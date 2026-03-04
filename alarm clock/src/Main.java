ce572a38f10662920199f8295cbdb8dd74f8b1ee
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-03-04 13:38:32 +0100

    first setup made

--- alarm clock/src/Main.java
+++ alarm clock/src/Main.java
@@ -1,5 +1,31 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime;
+
+        try{
+
+            System.out.println("Welcome to the Alarm Clock!");
+            System.out.print("Please enter your alarm time (HH:mm:ss): ");
+            String inputTime = scanner.nextLine();
+
+            alarmTime = LocalTime.parse(inputTime, formatter);
+            System.out.println("alarm set for " + alarmTime);
+
+
+        }
+        catch (DateTimeParseException e){
+            System.out.println("Invalid time format");
+        }
+
+        scanner.close();
     }
 }
