86a67186f1be280119e8ac0abf9b50c3db7c21dc
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-03-04 11:55:19 +0100

    update main.java

--- enums/src/Main.java
+++ enums/src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a day of the week? ");
+        String response = scanner.nextLine().toUpperCase();
+
+
+        try {
+            Day day = Day.valueOf(response);
+            System.out.println(day);
+
+            switch (day) {
+                case MONDAY,
+                     TUESDAY,
+                     WEDNESDAY,
+                     THURSDAY,
+                     FRIDAY -> System.out.println("It is a weekday");
+                case SATURDAY,
+                     SUNDAY -> System.out.println("It is the weekend");
+            }
+
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println("please enter a valid day");
+        }
+        scanner.close();
     }
 }
