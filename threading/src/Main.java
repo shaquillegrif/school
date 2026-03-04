a9d8053a2675aa5491502d771c451cc9d27117ff
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-03-04 13:21:20 +0100

    finish main.java

---
+++ threading/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread = new Thread(myRunnable);
+        thread.setDaemon(true);
+        thread.start();
+
+        System.out.println("You have 5 seconds to enter your name");
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name);
+
+        scanner.close();
+    }
+}
---
+++ threading/src/MyRunnable.java
@@ -0,0 +1,19 @@
+public class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            try {
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e) {
+                System.out.println("Thread interrupted");
+            }
+
+            if (i == 5){
+                System.out.println("time's up");
+                System.exit(0);
+            }
+        }
+    }
+}
