37775bdccae5ca257f3d38c0895d78ebf618e7d9
sgriffith1995 <97673124+shaquillegrif@users.noreply.github.com>
2026-03-04 13:31:44 +0100

    finish project

--- Multithreading/src/Main.java
+++ Multithreading/src/Main.java
@@ -1,2 +1,22 @@
 public class Main {
+    public static void main(String[] args) {
+
+        Thread thread1 =  new Thread(new MyRunnable("ping"));
+        Thread thread2 =  new Thread(new MyRunnable("pong"));
+
+        System.out.println("game start");
+
+        thread1.start();
+        thread2.start();
+
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            System.out.println("main thread interrupted");
+        }
+
+        System.out.println("game end");
+    }
 }
